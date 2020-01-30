175b8e3e0f92eebae6a628a6846316d5060a2823
Rafael Gomes <rafaelgomes.web@gmail.com>
2020-01-30 17:15:29 -0300

    Added semantic ui and fixed vunerabilities

---
+++ blog/src/components/PostList.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class PostList extends React.Component {
+    render() {
+        return(
+            <div>Post</div>
+         );
+    };
+};
+
+export default PostList;
